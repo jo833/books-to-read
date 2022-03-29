cc89dad1047c0c337dff92d063a63b85b7a29bb8
Julia Oghigian <77372272+jo833@users.noreply.github.com>
2022-03-29 11:05:26 -0500

    Update LoadScreen.java

--- LoadScreen.java
+++ LoadScreen.java
@@ -5,7 +5,7 @@ public class LoadScreen {
 		
 		public static void main(String[] args) throws Exception {
 	        System.out.println("Welcome to Best Books");
-	        List<BookDataInterface> books = new BookLoad().loadFile("MovieData.csv");
+	        List<BookDataInterface> books = new BookLoad().loadFile("Top100booksALLTIME.csv");
 	        BackEnd engine = new BackEnd();
 	        for(BookDataInterface book : books) engine.insert(book);
 	        FrontEndInterface ui = new FrontEnd();
